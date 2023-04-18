a8fd02c8ac509f479a2574899decb461bb3485e5
Your Name Here <gyon0004@student.monash.edu>
2023-04-18 12:49:04 +0800

    Uncompleted player classes (computer, human) that will be used as the players for the game

---
+++ src/game/Players/ComputerPlayer.java
@@ -0,0 +1,15 @@
+package game.Players;
+
+import game.Colours;
+
+/**
+ *  Class representing a AI player
+ *
+ * @author Garret Yong Shern Min
+ * @version 1.0 18/4/2023
+ */
+public class ComputerPlayer extends Player{
+    public ComputerPlayer() {
+        super(false, Colours.WHITE);
+    }
+}
---
+++ src/game/Players/HumanPlayer.java
@@ -0,0 +1,14 @@
+package game.Players;
+import game.Colours;
+
+/**
+ *  Class representing a human player
+ *
+ * @author Garret Yong Shern Min
+ * @version 1.0 18/4/2023
+ */
+public class HumanPlayer extends Player {
+    public HumanPlayer() {
+        super(false, Colours.BLACK);
+    }
+}
