f32127555d712508eb00cb5d78f29aeda4dac95c
Your Name Here <gyon0004@student.monash.edu>
2023-04-18 12:47:37 +0800

    Created temporary Goto Class (Menu, Game) not fully implemented (placeholder)

---
+++ src/game/Commands/GotoMenuCommand.java
@@ -0,0 +1,14 @@
+package game.Commands;
+
+/**
+ *  Class of Command to return to the menu
+ *
+ * @author Garret Yong Shern Min
+ * @version 1.0 18/4/2023
+ */
+public class GotoMenuCommand implements Command{
+    @Override
+    public void execute() {
+
+    }
+}
