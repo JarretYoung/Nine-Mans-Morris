a932be7fdf772d6f2f4aa3b45fe15addb0b97e22
Your Name Here <gyon0004@student.monash.edu>
2023-04-18 12:46:41 +0800

    Created a Command interface for future Command classes

---
+++ src/game/Commands/Command.java
@@ -0,0 +1,11 @@
+package game.Commands;
+
+/**
+ *  Interface for executable Commands
+ *
+ * @author Garret Yong Shern Min
+ * @version 1.0 18/4/2023
+ */
+public interface Command {
+    void execute();
+}
