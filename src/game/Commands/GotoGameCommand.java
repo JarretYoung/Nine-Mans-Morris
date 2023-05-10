3de5d9f0b7be0428342895bf1d02c5ae3db426b7
cong0014 <cong0014@student.monash.edu>
2023-05-10 17:36:54 +0800

    completed basic MainMenuPage, Button and created ExitCommand.

--- src/game/Commands/GotoGameCommand.java
+++ src/game/Commands/GotoGameCommand.java
@@ -1,14 +1,21 @@
 package game.Commands;
 
+import game.UIComponents.GamePage;
+import game.UIComponents.Panel;
+
 /**
  *  Class of Command to return to the game
  *
  * @author Garret Yong Shern Min
  * @version 1.0 18/4/2023
  */
-public class GotoGameCommand implements Command{
+public class GotoGameCommand implements Command {
+    private Panel panel;
+    public GotoGameCommand(Panel panel) {
+        this.panel = panel;
+    }
     @Override
     public void execute() {
-
+        this.panel.setCurrentPage(GamePage.ID);
     }
 }
