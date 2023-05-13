60018238fc5a396efabc7c8ff91b815f446d1dc1
Your Name Here <gyon0004@student.monash.edu>
2023-05-13 13:15:32 +0800

    Pushing base undo functions (to be changed)

--- src/UndoFunction/GameState.java
+++ src/UndoFunction/GameState.java
@@ -5,12 +5,18 @@ import game.Drawables.Token;
 import game.Players.Player;
 import game.UIComponents.Board;
 
+import java.util.ArrayList;
+
 public class GameState {
     private Board board;
+    private Integer playerOnePieces;
+    private Integer playerTwoPieces;
 
-    public GameState(Board board) {
-        this.board = board;
-    }
+//    public GameState(Board board, Integer playerOnePieces, Integer playerTwoPieces) {
+//        this.board = board;
+//        this.playerOnePieces = playerOnePieces;
+//        this.playerTwoPieces = playerTwoPieces;
+//    }
 
     public void placePiece(Player player, Position position) {
         position.setToken(new Token(position.getPage(),position.getX(),position.getY(),player.getTeam()));
@@ -27,5 +33,46 @@ public class GameState {
         position.getToken().delete();
         position.setToken(null);
         player.changePiecesLeft(-1);
+    }
+
+    public void set(Board board, Integer playerOnePieces, Integer playerTwoPieces) {
+        this.board = board;
+        this.playerOnePieces = playerOnePieces;
+        this.playerTwoPieces = playerTwoPieces;
+    }
+
+    public Memento takeSnapshot() {
+        return new Memento(this.board, this.playerOnePieces, this.playerTwoPieces);
+    }
+
+    public void restore(Memento memento) {
+        this.board = memento.getSavedBoardState();
+        this.playerOnePieces = memento.getPlayerOnePieces();
+        this.playerTwoPieces = memento.getPlayerTwoPieces();
+    }
+
+
+    public static class Memento {
+        private Board board;
+        private Integer playerOnePieces;
+        private Integer playerTwoPieces;
+
+        private Memento (Board _board, Integer playerOnePieces, Integer playerTwoPieces) {
+            this.board = _board;
+            this.playerOnePieces = playerOnePieces;
+            this.playerTwoPieces = playerTwoPieces;
+        }
+
+        private Board getSavedBoardState() {
+            return this.board;
+        }
+
+        private Integer getPlayerOnePieces() {
+            return this.playerOnePieces;
+        }
+
+        private Integer getPlayerTwoPieces() {
+            return this.playerTwoPieces;
+        }
     }
 }
---
+++ src/UndoFunction/GameStateEditor.java
@@ -0,0 +1,25 @@
+package UndoFunction;
+
+import game.UIComponents.Board;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class GameStateEditor {
+    private Deque<GameState.Memento> stateHistory;
+    private GameState gameState;
+
+    public GameStateEditor() {
+        stateHistory = new LinkedList<>();
+        gameState = new GameState();
+    }
+
+    public void makeMove(Board board, Integer playerOnePieces, Integer playerTwoPieces) {
+        gameState.set(board, playerOnePieces, playerTwoPieces);
+        stateHistory.add(gameState.takeSnapshot());
+    }
+
+    public void undo() {
+        gameState.restore(stateHistory.pop());
+    }
+}
